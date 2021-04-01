93bc479353bfa1d69f35b5228c64db638a50995c
alexanderyeremi <80661362+alexanderyeremi@users.noreply.github.com>
2021-04-01 08:35:46 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -2,10 +2,10 @@ let handler = async m => m.reply(`
 ╭─「 DONATION 」
 │• SAWERIA :
 │https://saweria.co/donate/RC047
-│• OVO [+62895337278647]
+│• OVO [+6281281783202]
 │• TRI [+62895337278647]
-│• DANA [+62895337278647]
-│• GOPAY [+62895337278647]
+│• DANA [+6281281783202]
+│• GOPAY [+6281281783202]
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
